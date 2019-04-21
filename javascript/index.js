d5fdf5ac2799f31222ba8bd7eb14386fdc9af7f2
samgoldman <sgoldman216@gmail.com>
2019-04-21 13:21:46 -0400

    Display images with paths

--- javascript/index.js
+++ javascript/index.js
@@ -5,17 +5,129 @@ var floors = {"A": "L2",
 			  "C": "G",
 			  "D": "1",
               "E": "2",
-			  "F": "3"};
+			  "F": "3",
+              "G": "4"};
 
 function getDirectionTokens() {
 	let value = getQueryVariable("dirs");
 	return value.split(',');
 }
 
+function generateSVG(segId) {
+	let pathTemplate = "<path style='fill: none;stroke: {color};stroke-width: 10px;' d='{coords}'/>";
+	let svg = "<svg xmlns='http://www.w3.org/2000/svg' width='350' height='238' viewBox='{translateX} {translateY} 350 238'><defs><pattern id='img{id}' width='5000px' height='3400px' patternUnits='userSpaceOnUse'><image href='{image}'/></pattern></defs><g transform='scale(.2)'><rect width='5000' height='3400' style='fill:url(#img{id})'/></g><g transform='scale(.2)'>{path}</g></svg>";
+
+	let id = Math.floor(Math.random() * 9999999999999);
+
+	svg = svg.replaceAll("{id}", id);
+
+	let floor = "";
+	let segCountForId = -1;
+	let x1, y1, x2, y2;
+
+	let segCount = 1;
+	let edgeCount = 0;
+
+	let svgPathsBySegment = {};
+
+	for (const key of Object.keys(path)) {
+		let floorSet = path[key];
+		let thisFloor = key.split("-")[0];
+
+		let pathCoords = "";
+
+		for(let i = 0; i < floorSet.length; i++) {
+			if(segCount - 2 === segId && edgeCount === 0) {
+				x1 = floorSet[i].x;
+				y1 = floorSet[i].y;
+				floor = thisFloor;
+				segCountForId = segments[segCount];
+
+				pathCoords = "M" + floorSet[i].x + " " + floorSet[i].y;
+			} else if(segCount - 2 === segId) {
+				pathCoords += "L" + floorSet[i].x + " " + floorSet[i].y;
+			} else if(edgeCount === 0) {
+				pathCoords = "M" + floorSet[i].x + " " + floorSet[i].y;
+			} else {
+				pathCoords += "L" + floorSet[i].x + " " + floorSet[i].y;
+			}
+
+			edgeCount++;
+
+			if(edgeCount === segments[segCount]) {
+				if(segCount - 2 === segId) {
+					x2 = floorSet[i].x;
+					y2 = floorSet[i].y;
+				}
+				if(i < floorSet.length -1)
+				pathCoords += "L" + floorSet[i+1].x + " " + floorSet[i+1].y;
+
+				if (svgPathsBySegment[key] === undefined) {
+					svgPathsBySegment[key] = "";
+				}
+
+				if(segCount - 2 === segId) {
+					svgPathsBySegment[key] += pathTemplate.replace("{color}", "#F00").replace("{coords}", pathCoords);
+				} else {
+					svgPathsBySegment[key] += pathTemplate.replace("{color}", "#000").replace("{coords}", pathCoords);
+				}
+				pathCoords = "";
+
+				segCount++;
+				edgeCount = 0;
+			}
+		}
+	}
+
+	let svgPath = "";
+	for (const key of Object.keys(svgPathsBySegment)) {
+		let svgPathSegment = svgPathsBySegment[key];
+		let thisFloor = key.split("-")[0];
+		if (thisFloor === floor) {
+			svgPath += svgPathSegment;
+		}
+	}
+
+	svg = svg.replace("{path}", svgPath);
+
+	let centerX = (x2 + x1) / 2;
+	let centerY = (y2 + y1) / 2;
+
+	svg = svg.replace("{translateX}", "" + ((centerX / 5) - 350/2));
+	svg = svg.replace("{translateY}", "" + ((centerY / 5) - 238/2));
+
+	switch(floor) {
+		case "1":
+			svg = svg.replace("{image}", "images/F1.png");
+			break;
+		case "2":
+			svg = svg.replace("{image}", "images/F2.png");
+			break;
+		case "3":
+			svg = svg.replace("{image}", "images/F3.png");
+			break;
+		case "4":
+			svg = svg.replace("{image}", "images/F4.png");
+			break;
+		case "G":
+			svg = svg.replace("{image}", "images/ground.png");
+			break;
+		case "L1":
+			svg = svg.replace("{image}", "images/L1.png");
+			break;
+		case "L2":
+			svg = svg.replace("{image}", "images/L2.png");
+			break;
+	}
+
+	return svg;
+}
+
 function generateDirs() {
 	let i = 0;
 	let tokens = getDirectionTokens();
 	let directions = [];
+	let j = 0;
 	for (i = 0; i < tokens.length; i++) {
 		let token = tokens[i];
 
@@ -23,6 +135,7 @@ function generateDirs() {
 
 		token = token.replaceAll("$", " ");
 		let direction = {};
+		let isElevatorOrStairs = false;
 		switch(token.substring(0,1)) {
 			case "A":
 				direction.text = "Walk straight for " + parseInt(token.substring(1, 6)) + " ft. " + token.substring(6);
@@ -67,18 +180,22 @@ function generateDirs() {
 			case "N":
 				direction.text = "Take the elevator up from floor " + floors[token.substring(1,2)] + " to floor " + floors[token.substring(2,3)];
 				direction.image = "elevator.svg";
+				isElevatorOrStairs = true;
 				break;
 			case "O":
 				direction.text = "Take the elevator down from floor " + floors[token.substring(1,2)] + " to floor " + floors[token.substring(2,3)];
 				direction.image = "elevator.svg";
+				isElevatorOrStairs = true;
 				break;
 			case "P":
 				direction.text = "Take the stairs up from floor " + floors[token.substring(1,2)] + " to floor " + floors[token.substring(2,3)];
 				direction.image = "stairs-up.svg";
+				isElevatorOrStairs = true;
 				break;
 			case "Q":
 				direction.text = "Take the stairs down from floor " + floors[token.substring(1,2)] + " to floor " + floors[token.substring(2,3)];
 				direction.image = "stairs-up.svg";
+				isElevatorOrStairs = true;
 				break;
 			case "S":
 				direction.text = "Walk north for " + parseInt(token.substring(1, 6)) + " ft. " + token.substring(6);
@@ -122,21 +239,46 @@ function generateDirs() {
 		let table = document.getElementById("dir_table");
 		let caro = document.getElementById("carousel");
 		if (direction.image !== undefined) {
-			table.innerHTML += "\n" +
-				"        <tr>\n" +
-				"          <td scope=\"row\"><img width='20px' height='20px' src='images\\" + direction.image + "' \></td>\n" +
-				"          <td>" + direction.text + "</td>\n" +
-				"        </tr>";
+			if (isElevatorOrStairs) {
+				table.innerHTML += "\n" +
+					"        <tr>\n" +
+					"          <td scope=\"row\"><img width='20px' height='20px' src='images\\" + direction.image + "' \></td>\n" +
+					"          <td>" + direction.text + "</td>\n" +
+					"        </tr>";
 
-			if (i === 0) {
-				caro.innerHTML += "<div class='carousel-item active'> <img style='max-width: 100%; max-height: 100%' src='images\\"
-					+ direction.image +
-					"'> <svg width='100%' height='20%'><rect fill='#fff' width='100%' height='20%'></rect></svg><div class='carousel-caption'> <h5>"
-					+ direction.text
-					+ "</h5> </div>"
+
+				if (i === 0) {
+					caro.innerHTML += "<div class='carousel-item active'> <img style='max-width: 100%; max-height: 100%' src='images\\"
+						+ direction.image +
+						"'> <svg width='100%' height='20%'><rect fill='#fff' width='100%' height='20%'></rect></svg><div class='carousel-caption'> <h5>"
+						+ direction.text
+						+ "</h5> </div>"
+				} else {
+					caro.innerHTML += "<div class=\"carousel-item\"> <img style=\"max-width: 100%; max-height: 100%\" src='images\\" + direction.image + "'> <svg fill='#fff' width='100%' height='20%'><rect width='100%' height='20%'></rect></svg><div class=\"carousel-caption\"> <h5>" + direction.text + "</h5> </div>"
+				}
 			} else {
-				caro.innerHTML += "<div class=\"carousel-item\"> <img style=\"max-width: 100%; max-height: 100%\" src='images\\" + direction.image + "'> <svg fill='#fff' width='100%' height='20%'><rect width='100%' height='20%'></rect></svg><div class=\"carousel-caption\"> <h5>" + direction.text + "</h5> </div>"
+				table.innerHTML += "\n" +
+					"        <tr class='view'>\n" +
+					"          <td scope=\"row\"><img width='20px' height='20px' src='images\\" + direction.image + "' \></td>\n" +
+					"          <td>" + direction.text + "</td>\n" +
+					"        </tr>" +
+					"		<tr class='fold'><td colspan=\"7\">\n" +
+					"        <div class=\"fold-content\">" +
+					generateSVG(j) +
+					"</div></td></tr>";
+
+
+
+
+				if (i === 0) {
+					caro.innerHTML += `<div class='carousel-item active'> ${generateSVG(j)} <svg width='100%' height='20%'><rect fill='#fff' width='100%' height='20%'></rect></svg><div class='carousel-caption'> <h5>${direction.text}</h5></div>`
+				} else {
+					caro.innerHTML += `<div class="carousel-item">${generateSVG(j)}<svg fill='#fff' width='100%' height='20%'><rect width='100%' height='20%'></rect></svg><div class="carousel-caption"> <h5>${direction.text}</h5> </div>`
+				}
+				j++;
 			}
+
+
 		} else {
 			table.innerHTML += "\n" +
 				"        <tr>\n" +
@@ -144,5 +286,63 @@ function generateDirs() {
 				"          <td>" + direction.text + "</td>\n" +
 				"        </tr>";
 		}
+
+
+
+		$(function(){
+			$(".fold-table tr.view").on("click", function(){
+				$(this).toggleClass("open").next(".fold").toggleClass("open");
+			});
+		});
 	}
 }
+
+let scale = 5;
+
+function parsePath() {
+	let params = getQueryVariable("dirs").split(",");
+
+	let i = 0;
+	while(params[i] !== ";") i++;
+	i++;
+
+	let pathByFloor = {};
+
+	for(i; i < params.length; i++) {
+		let value = params[i];
+
+		if (value === ";") break;
+
+		let data = value.split(";");
+		let x = parseInt(data[0]);
+		let y = parseInt(data[1]);
+		let floor = data[2];
+
+		if (pathByFloor[floor] === undefined) {
+			pathByFloor[floor] = [];
+		}
+		pathByFloor[floor].push({"x": x, "y": y});
+	}
+
+	return pathByFloor;
+}
+
+function parseSegments() {
+	let params = getQueryVariable("dirs").split(",");
+
+	let i = 0;
+	while(params[i] !== ";") i++;
+	i++;
+	while(params[i] !== ";") i++;
+	i++;
+
+	let segments = [];
+	for(i; i < params.length; i++) {
+		segments.push(parseInt(params[i]));
+	}
+
+	return segments;
+}
+
+let path = parsePath();
+let segments = parseSegments();
--- javascript/map.js
+++ javascript/map.js
@@ -8,10 +8,6 @@ let g = new Image;
 let l1 = new Image;
 let l2 = new Image;
 
-
-let tempCanvas=document.createElement("canvas");
-let tctx=tempCanvas.getContext("2d");
-
 /// start image loading, when done draw and setup
 f2.onload = start;
 f1.src = 'images/F1.png';
@@ -21,6 +17,8 @@ f4.src = 'images/F4.png';
 g.src = 'images/ground.png';
 l1.src = 'images/L1.png';
 l2.src = 'images/L2.png';
+
+let scale = 5;
 
 function parsePath() {
 	let params = getQueryVariable("dirs").split(",");
@@ -33,6 +31,9 @@ function parsePath() {
 
 	for(i; i < params.length; i++) {
 		let value = params[i];
+
+		if (value === ";") break;
+
 		let data = value.split(";");
 		let x = parseInt(data[0]);
 		let y = parseInt(data[1]);
@@ -47,11 +48,31 @@ function parsePath() {
 	return pathByFloor;
 }
 
+function parseSegments() {
+	let params = getQueryVariable("dirs").split(",");
+
+	let i = 0;
+	while(params[i] !== ";") i++;
+	i++;
+	while(params[i] !== ";") i++;
+	i++;
+
+	let segments = [];
+	for(i; i < params.length; i++) {
+		segments.push(parseInt(params[i]));
+	}
+
+	segments.unshift(1);
+
+	return segments;
+}
+
 let path = parsePath();
+let segments = parseSegments();
 
 function start() {
 	/// initial draw of image
-	ctx.drawImage(f2, 0, 0, 500, 340);
+	ctx.drawImage(f2, 0, 0, 5000/scale, 3400/scale);
 	drawPath("2");
 }
 
@@ -59,25 +80,25 @@ function switchFloor(floor) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	switch (floor) {
 		case "1":
-			ctx.drawImage(f1, 0, 0, 500, 340);
+			ctx.drawImage(f1, 0, 0, 5000/scale, 3400/scale);
 			break;
 		case "2":
-			ctx.drawImage(f2, 0, 0, 500, 340);
+			ctx.drawImage(f2, 0, 0, 5000/scale, 3400/scale);
 			break;
 		case "3":
-			ctx.drawImage(f3, 0, 0, 500, 340);
+			ctx.drawImage(f3, 0, 0, 5000/scale, 3400/scale);
 			break;
 		case "4":
-			ctx.drawImage(f4, 0, 0, 500, 340);
+			ctx.drawImage(f4, 0, 0, 5000/scale, 3400/scale);
 			break;
 		case "G":
-			ctx.drawImage(g, 0, 0, 500, 340);
+			ctx.drawImage(g, 0, 0, 5000/scale, 3400/scale);
 			break;
 		case "L1":
-			ctx.drawImage(l1, 0, 0, 500, 340);
+			ctx.drawImage(l1, 0, 0, 5000/scale, 3400/scale);
 			break;
 		case "L2":
-			ctx.drawImage(l2, 0, 0, 500, 340);
+			ctx.drawImage(l2, 0, 0, 5000/scale, 3400/scale);
 			break;
 	}
 	drawPath(floor);
@@ -89,25 +110,42 @@ $(window).resize(function(){
 	canvas.height(canvas.width() / 1.471);
 });
 
+let colors = ["RED", "BLUE", "BLACK", "GREEN"];
+
 function drawPath(floorToDraw) {
+	let segCount = 3;
+	let edgeCount = 0;
 	Object.keys(path).forEach(function(key) {
 		let segment = path[key];
 		let floor = key.split("-")[0];
 
-		if (floor === floorToDraw && segment.length > 0) {
-			ctx.beginPath();
+		if (segment.length > 0) {
 
 			let point = segment[0];
 
-			ctx.moveTo(point.x/10.0, point.y/10.0);
-
 			for(let i = 1; i < segment.length; i++) {
-				point = segment[i];
-				ctx.lineTo(point.x/10, point.y/10);
+				let point2 = segment[i];
+
+				if (floor === floorToDraw) {
+					ctx.beginPath();
+					ctx.moveTo(point.x / scale, point.y / scale);
+					ctx.lineTo(point2.x / scale, point2.y / scale);
+					ctx.lineWidth = 2;
+					ctx.strokeStyle = colors[segCount % 4];
+					ctx.stroke();
+				}
+
+				point = point2;
+
+				edgeCount++;
+
+				if(edgeCount === segments[segCount]) {
+					segCount++;
+					edgeCount = 0;
+				}
+
+
 			}
-			ctx.lineWidth = 1;
-			ctx.strokeStyle = "BLUE";
-			ctx.stroke();
 		}
 	});
 }
